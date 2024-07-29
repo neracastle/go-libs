df9905bc89869defdabe8d0b108ef103d9f56ac9
Ivan Semeniv <neracastle@gmail.com>
2024-07-29 16:05:34 +0300

    initial

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/neracastle/go-libs
+
+go 1.20
+
+require (
+	github.com/jackc/pgx/v5 v5.6.0
+	github.com/neracastle/auth v0.0.0-20240726122254-32c2b390abf8
+	github.com/pkg/errors v0.9.1
+	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56
+)
+
+require (
+	github.com/gomodule/redigo v1.9.2 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
---
+++ pkg/redis/client.go
@@ -0,0 +1,20 @@
+package redis
+
+import (
+	"context"
+	"time"
+)
+
+// Client Клиент-обертка к редису
+type Client interface {
+	Set(ctx context.Context, key string, value interface{}) error
+	SetEx(ctx context.Context, key string, value interface{}, expiration time.Duration) error
+	Get(ctx context.Context, key string) (string, error)
+	Del(ctx context.Context, key string) error
+	Expire(ctx context.Context, key string, expiration time.Duration) error
+	HSet(ctx context.Context, key string, field string, value string) error
+	HSetMap(ctx context.Context, key string, values interface{}) error
+	HGet(ctx context.Context, key string, field string) (string, error)
+	HDel(ctx context.Context, key string, field string) error
+	HGetAll(ctx context.Context, key string, dest interface{}) error
+}
